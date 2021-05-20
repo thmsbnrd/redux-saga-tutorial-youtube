1473072da9ad84feaaa1fdcd35e2e3173b934e17
Thomas BENARD <thomas0benard@gmail.com>
2021-05-20 14:56:49 +0200

    basic tutorial done

--- src/modules/posts/Posts.reducer.js
+++ src/modules/posts/Posts.reducer.js
@@ -1,3 +1,6 @@
+/* eslint-disable import/no-anonymous-default-export */
+import { GET_POSTS, GET_POSTS_SUCCESS } from "./Posts.actions";
+
 const initialState = {
   isLoading: false,
   posts: [],
@@ -6,6 +9,17 @@ const initialState = {
 
 export default (state = initialState, { type, payload }) => {
   switch (type) {
+    case GET_POSTS:
+      return {
+        ...state,
+        isLoading: true,
+      }
+    case GET_POSTS_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        posts: payload,
+      }
     default:
       return state;
   }
